d2019717a5729b6d4aca7ffbb4e3ec1d3d0ef914
yanbasic <yanbasic@gmail.com>
2019-05-10 16:17:08 +0800

    fix(TESB-24663): Remove "DOCTYPE" declaration for camel components

---
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/generator/LanguageComboController.java
@@ -0,0 +1,144 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.camel.designer.generator;
+
+import org.talend.core.model.process.EParameterFieldType;
+import org.talend.core.model.process.IElementParameter;
+import org.talend.core.model.process.IElementParameterDefaultValue;
+import org.talend.core.ui.properties.tab.IDynamicProperty;
+import org.talend.designer.core.model.components.ElementParameterDefaultValue;
+import org.talend.designer.core.ui.editor.properties.controllers.ComboController;
+
+/**
+ * Camel language combo list controller
+ */
+public class LanguageComboController extends ComboController {
+
+    private static String[] displayCodeNames = {
+	    "NONE",
+	    "BEAN",
+	    "CONSTANT",
+	    "CORRELATIONID",
+	    "EL",
+	    "GROOVY",
+	    "HEADER",
+	    "JAVASCRIPT",
+	    "JOSQL",
+	    "JSONPATH",
+	    "JXPATH",
+	    "MVEL",
+	    "OGNL",
+	    "PHP",
+	    "PROPERTY",
+	    "PYTHON",
+	    "RUBY",
+	    "SIMPLE",
+	    "SPEL",
+	    "SQL",
+	    "XPATH",
+	    "XQUERY"
+	  };
+  
+    private static String[] itemValues = {
+	    "none",
+	    "bean",
+	    "constant",
+	    "correlation",
+	    "el",
+	    "groovy",
+	    "header",
+	    "javaScript",
+	    "josql",
+	    "jsonpath",
+	    "jxpath",
+	    "mvel",
+	    "ognl",
+	    "php",
+	    "property",
+	    "python",
+	    "ruby",
+	    "simple",
+	    "spel",
+	    "sql",
+	    "xpath",
+	    "xquery"
+	  };
+    
+    private static String[] displayNames = {
+	    "None",
+	    "Bean",
+	    "Constant",
+	    "CorrelationID",
+	    "EL",
+	    "Groovy",
+	    "Header",
+	    "JavaScript",
+	    "JoSQL",
+	    "JSonPath",
+	    "JXPath",
+	    "MVEL",
+	    "OGNL",
+	    "PHP",
+	    "Property",
+	    "Python",
+	    "Ruby",
+	    "Simple",
+	    "SpEl",
+	    "SQL",
+	    "XPath",
+	    "XQuery"
+	  };
+    
+    public LanguageComboController(IDynamicProperty dp) {
+	super(dp);
+	for (IElementParameter param : elem.getElementParameters()) {
+	    if (EParameterFieldType.LANGUAGE_CLOSED_LIST.equals(param.getFieldType())) {
+		addLanguageList(param);
+	    } else if (EParameterFieldType.TABLE.equals(param.getFieldType())) {
+		for (Object item : param.getListItemsValue()) {
+		    if (item instanceof IElementParameter) {
+			IElementParameter itemParam = (IElementParameter) item;
+			if ("LANGUAGE".equals(itemParam.getName())) {
+			    addLanguageList(itemParam);
+			}
+		    }
+		}
+	    }
+	}
+    }
+    
+    private void addLanguageList(IElementParameter param) {
+	if (param.getListItemsValue() == null || param.getListItemsValue().length == 0) {
+	    param.setListItemsValue(itemValues);
+	    param.setListItemsDisplayCodeName(displayCodeNames);
+	    param.setListItemsDisplayName(displayNames);
+	    param.setListRepositoryItems(new String[itemValues.length]);
+	    param.setListItemsShowIf(new String[itemValues.length]);
+	    param.setListItemsNotShowIf(new String[itemValues.length]);
+	    param.setListItemsReadOnlyIf(new String[itemValues.length]);
+	    param.setListItemsNotReadOnlyIf(new String[itemValues.length]);
+
+	    if (param.getValue() == null || "".equals(param.getValue())) {
+		param.setDefaultClosedListValue(itemValues[2]); // constant
+	    } else {
+		for (int i = 0; i < displayCodeNames.length; i++) {
+		    if (displayCodeNames[i].equals(param.getValue())) {
+			param.setValue(displayNames[i]);
+			break;
+		    }
+		}
+	    }
+	}
+	param.setFieldType(EParameterFieldType.CLOSED_LIST);
+    }
+}
---
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/generator/LanguageComboGenerator.java
@@ -0,0 +1,36 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.camel.designer.generator;
+
+import org.talend.core.ui.properties.tab.IDynamicProperty;
+import org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController;
+import org.talend.designer.core.ui.editor.properties.controllers.generator.ComboGenerator;
+
+/**
+ * @author yyan
+ *
+ */
+public class LanguageComboGenerator extends ComboGenerator {
+
+    private IDynamicProperty dp;
+
+    @Override
+    public AbstractElementPropertySectionController generate() {
+	return new LanguageComboController(dp);
+    }
+
+    @Override
+    public void setDynamicProperty(IDynamicProperty dp) {
+	this.dp = dp;
+    }
+}
