26184a97189b60825649f0a6a2d5e14b36e9f95e
Maksym Sheverda <maksym.sheverda@synapse.com>
2020-04-22 14:58:24 +0300

    TESB-28895 Route node is missing in project setting

--- main/plugins/org.talend.resources.export.route/src/org/talend/resources/export/route/setting/repository/RouteDesignsRepositorySettingTester.java
+++ main/plugins/org.talend.resources.export.route/src/org/talend/resources/export/route/setting/repository/RouteDesignsRepositorySettingTester.java
@@ -2,8 +2,6 @@ package org.talend.resources.export.route.setting.repository;
 
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.designer.maven.ui.setting.repository.tester.AbstractRepositoryNodeRepositorySettingTester;
-import org.talend.designer.maven.ui.setting.repository.tester.IRepositorySettingTester;
-import org.talend.repository.model.IRepositoryNode;
 
 public class RouteDesignsRepositorySettingTester extends AbstractRepositoryNodeRepositorySettingTester {
 
