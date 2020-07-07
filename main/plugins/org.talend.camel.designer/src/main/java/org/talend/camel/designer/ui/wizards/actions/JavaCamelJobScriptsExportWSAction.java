5d3ea68981d8a6b3d1ece9dee8d606a0fc6d0379
apoltavtsev <apoltavtsev@gmail.com>
2020-07-07 09:22:04 +0200

    fix(TESB-29503) Bundle version does not use 'Custom Version' (#787)
    * fix(TESB-29503) Bundle version does not use 'Custom Version'
    
    * Update JavaCamelJobScriptsExportWSAction.java

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/actions/JavaCamelJobScriptsExportWSAction.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/actions/JavaCamelJobScriptsExportWSAction.java
@@ -301,7 +301,7 @@ public class JavaCamelJobScriptsExportWSAction implements IRunnableWithProgress 
                 final Set<String> routelets = new HashSet<>();
                 exportAllReferenceRoutelets(routeName, routeProcess, routelets);
 
-                exportRouteBundle(routeObject, routeFile, version, null, null, bundleVersion, null, routelets, null);
+                exportRouteBundle(routeObject, routeFile, version, null, null, routeVersion, null, routelets, null);
             }
 
             try {
@@ -644,8 +644,9 @@ public class JavaCamelJobScriptsExportWSAction implements IRunnableWithProgress 
                         new BundleModel(routeletModelGroupId, routeletBundleName, routeletModelVersion, routeletFile);
 
                 if (featuresModel.addBundle(routeletModel)) {
+                    String routeletBundleVersion = getArtifactVersion();
                     exportRouteBundle(referencedRouteletNode, routeletFile, routeletVersion, routeletBundleName,
-                            routeletBundleSymbolicName, bundleVersion, idSuffix, null,
+                            routeletBundleSymbolicName, routeletBundleVersion, idSuffix, null,
                             EmfModelUtils
                                     .findElementParameterByName(EParameterName.PROCESS_TYPE.getName() + ':'
                                             + EParameterName.PROCESS_TYPE_CONTEXT.getName(), node)
