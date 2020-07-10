3372d0922348f2b1b7e336fe37f7f3f1b2d40f67
Zhiwei Xue <zwxue@talend.com>
2020-07-10 15:39:22 +0800

    fix(TUP-27610): Clean up the CI version everywhere and have a common place to setup in studio

--- main/plugins/org.talend.repository.services/src/org/talend/repository/services/maven/CreateMavenDataServicePom.java
+++ main/plugins/org.talend.repository.services/src/org/talend/repository/services/maven/CreateMavenDataServicePom.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.BooleanUtils;
@@ -34,6 +33,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.EList;
+import org.talend.commons.utils.MojoType;
 import org.talend.commons.utils.VersionUtils;
 import org.talend.core.model.process.JobInfo;
 import org.talend.core.model.properties.Project;
@@ -337,9 +337,9 @@ public class CreateMavenDataServicePom extends CreateMavenJobPom {
     private Plugin addOsgiHelperMavenPlugin() {
         Plugin plugin = new Plugin();
 
-        plugin.setGroupId("org.talend.ci");
-        plugin.setArtifactId("osgihelper-maven-plugin");
-        plugin.setVersion(VersionUtils.getMojoVersion("osgihelper.version"));
+        plugin.setGroupId(TalendMavenConstants.DEFAULT_CI_GROUP_ID);
+        plugin.setArtifactId(MojoType.OSGI_HELPER.getArtifactId());
+        plugin.setVersion(VersionUtils.getMojoVersion(MojoType.OSGI_HELPER));
 
         Xpp3Dom configuration = new Xpp3Dom("configuration");
         Xpp3Dom featuresFile = new Xpp3Dom("featuresFile");
