1fd6df1057db857957e724561cb5d7a6886a716b
xldai <xldai@talend.com>
2021-06-03 18:53:12 +0800

    chore(APPINT-32986) fix CVE commons-io 2.6

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/build/CreateMavenBundlePom.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/build/CreateMavenBundlePom.java
@@ -404,6 +404,11 @@ public class CreateMavenBundlePom extends CreateMavenJobPom {
         mavensharedDep.setArtifactId("maven-shared-utils");
         mavensharedDep.setVersion("3.3.3");
 
+        Dependency commonsioDep = new Dependency();
+        commonsioDep.setGroupId("commons-io");
+        commonsioDep.setArtifactId("commons-io");
+        commonsioDep.setVersion("2.8.0");
+
         Dependency httpclientDep = new Dependency();
         httpclientDep.setGroupId("org.apache.httpcomponents");
         httpclientDep.setArtifactId("httpclient");
@@ -425,6 +430,7 @@ public class CreateMavenBundlePom extends CreateMavenJobPom {
         junitDep.setVersion("4.13.2");
 
         dependencies.add(mavensharedDep);
+        dependencies.add(commonsioDep);
         dependencies.add(httpclientDep);
         dependencies.add(httpcoreDep);
         dependencies.add(istackDep);
