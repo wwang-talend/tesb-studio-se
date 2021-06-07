458a2deb46f429f0bccefe6559b38aa23c6acfd7
xldai <xldai@talend.com>
2021-06-07 13:57:08 +0800

    APPINT-33034 update the UpdateMQTTMigrationTask and correct the MQTT_VERSION

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/migration/UpdateMQTTMigrationTask.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/migration/UpdateMQTTMigrationTask.java
@@ -1,6 +1,22 @@
 package org.talend.camel.designer.migration;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 
 public class UpdateMQTTMigrationTask extends AbstractJobMigrationTask {
 
@@ -21,11 +37,16 @@ public class UpdateMQTTMigrationTask extends AbstractJobMigrationTask {
                             public void transform(NodeType node) {
                                 ElementParameterType reConnDelay = ComponentUtilities.getNodeProperty(node, "MQTT_RECONNECT_DELAY");
 
-                                if (useProxy == null) {//$NON-NLS-1$
+                                if (reConnDelay == null) {//$NON-NLS-1$
                                     return;
                                 }
 
-                                //todo: set the value to MQTT_MAX_RECONNECT_DELAY
+                                // migrate "MQTT_RECONNECT_DELAY" to "MQTT_MAX_RECONNECT_DELAY"
+                                ElementParameterType maxReconnDelay =
+                                        ComponentUtilities.getNodeProperty(node, "MQTT_MAX_RECONNECT_DELAY"); //$NON-NLS-1$
+                                if (reConnDelay.getValue() != null) { //$NON-NLS-1$
+                                    maxReconnDelay.setValue(reConnDelay.getValue()); //$NON-NLS-1$
+                                }
                                 
                             }
                         }));
@@ -40,7 +61,7 @@ public class UpdateMQTTMigrationTask extends AbstractJobMigrationTask {
     }
 
     public Date getOrder() {
-        GregorianCalendar gc = new GregorianCalendar(2021, 2, 22, 13, 0, 0);
+        GregorianCalendar gc = new GregorianCalendar(2021, 6, 7, 13, 0, 0);
         return gc.getTime();
     }
 
