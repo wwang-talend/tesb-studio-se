bb3a58dc4c08cd324ab8832a6a7173724f95324c
xldai <xldai@talend.com>
2021-07-08 16:41:30 +0800

    chore(APPINT-33373) add UpdateCHTTPUriMigrationTask

---
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/migration/UpdateCHTTPUriMigrationTask.java
@@ -0,0 +1,49 @@
+package org.talend.camel.designer.migration;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+public class UpdateCHTTPUriMigrationTask extends AbstractRouteItemComponentMigrationTask {
+
+    @Override
+    public String getComponentNameRegex() {
+        return "cHttp";
+    }
+
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2021, 7, 8, 13, 0, 0);
+        return gc.getTime();
+    }
+
+	@Override
+	protected boolean execute(NodeType node) throws Exception {
+		return replaceUri(node);
+	}
+
+	private boolean replaceUri(NodeType currentNode) throws PersistenceException {
+        boolean needSave = false;
+
+        ElementParameterType param = UtilTool.findParameterType(currentNode, "URI");
+        String value = param.getValue();
+
+        for (Object e : currentNode.getElementParameter()) {
+            ElementParameterType p = (ElementParameterType) e;
+            if ("URI".equals(p.getName()) && value.startsWith("\"jetty://")) {
+                value = value.replaceFirst("\"jetty://", "\"");
+                if (!value.startsWith("\"http://")){
+                    value = value.substring(value.indexOf("\"http://"));
+                }
+                p.setValue(value);
+                needSave = true;
+                break;
+            }
+        }
+
+        return needSave;
+    }
+
+}
